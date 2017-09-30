c33be039dcbba175f70b3bd1619f369eed90c1df
Jon Vollmer <jvollmer@Jons-MBP.home.local>
2017-09-30 11:48:35 -0500

    added edit user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,24 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			flash[:success] = "Welecome to the alpha blog #{@user.username}"
+			flash[:success] = "Welecome to the alpha blog, #{@user.username}"
 			redirect_to articles_path
 		else
 			render 'new'
+		end
+	end
+	
+	def edit
+		@user = User.find(params[:id])
+	end
+	
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:success] = "Your account was successfully updated"
+			redirect_to articles_path
+		else
+			render 'edit'
 		end
 	end
 	
