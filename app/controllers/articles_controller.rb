2e40cab08e862ad8f7700bdc5d51de35a7d3fde1
Jon Vollmer <jvollmer@Jons-MBP.home.local>
2017-09-30 13:50:26 -0500

    finished user authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-  
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -14,7 +15,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = current_user
     if @article.save
       flash[:success]= "Article was successfully created"
       redirect_to article_path(@article)
@@ -49,5 +50,9 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
-  
+	
+	def require_same_user
+		flash[:danger] = "You can only access your own articles"
+		redirect_to root_path
+	end
 end
